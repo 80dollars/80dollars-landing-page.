59bec5dec9d0c0c1cc76961513cb8f1f0742ea55
80dollars <admin@80dollars.com>
2026-04-02 15:24:15 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: '80 Dollars - Premium Domain',
+  description: 'This premium domain is for sale',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
